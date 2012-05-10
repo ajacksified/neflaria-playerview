97bdd5d30cfd4969efb936e6f505d9caa0e93ead
Jack Lawson <ajacksified@gmail.com>
2012-05-09 22:19:44 -0700

    load from hash

--- js/script.js
+++ js/script.js
@@ -2,19 +2,30 @@
   $(function(){
     var template = $("#playerTemplate").html();
 
-    $("form").bind("submit", function(e){
-      e.preventDefault();
-
+    var loadData = function(playerName){
       $.ajax({
         url: 'http://web1.neflaria.com/api/player.php',
         dataType: 'jsonp',
-        data: { player: $("#player").val() },
+        data: { player: playerName },
         success: function(data){
+          window.location.hash = $("#player").val();
           var templateData  = { player: data };
 
           $("#playerInfo").html(Mustache.render(template, templateData));
         }
       });
+    }
+
+    $("form").bind("submit", function(e){
+      e.preventDefault();
+      loadData($("#player").val());
     });
+
+    if(window.location.hash){
+      player = window.location.hash.substr(1,window.location.hash.length);
+
+      $("#player").val(player);
+      loadData(player);
+    }
   });
 })(window, jQuery);
